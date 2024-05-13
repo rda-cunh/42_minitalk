82987f99e565727b004643d5a6dd4e26846a8ff7
Ricardo Mendes <147749667+rda-cunh@users.noreply.github.com>
2024-05-13 16:44:56 +0000

    new version

--- client.c
+++ client.c
@@ -6,13 +6,32 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 14:05:48 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/13 13:11:16 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/13 16:43:49 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h> // remove after testing and including libft functions
 
+int main(int argc, char **argv)
+{
+    int i;
+    int bitshift;
+    char    word; 
+    
+    if (argc != 2)
+        return (1); 
+    word = argv[1][0];
+    bitshift = -1; 
+    while (++bitshift < 8)
+    {
+        if (word & 0x80 >> bitshift)
+            printf("1");
+        
+    } 
+}
+
+/*
 int main(int argc, char **argv)
 {
     int pid;
@@ -26,3 +45,4 @@ int main(int argc, char **argv)
     kill(pid, SIGUSR1);
     return (0); 
 }
+*/
