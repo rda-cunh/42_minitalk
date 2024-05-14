1a24246a0a5a66074e2de706771a8d1457cbf9c6
rda-cunh <ricard.mendes@gmail.com>
2024-05-14 18:47:23 +0100

    new version untested

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 14:05:48 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/14 18:28:14 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/05/14 18:46:29 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,22 @@ void    signal_sender(char *message, int pid)
 {
     int i;
     int bitshift;
-    char word;
 
     i = 0; 
     bitshift = -1;
-    while  
-
-
-    while(++bitshift < 8)
+    while (message[i])
     {
-        if (word & 0x80 >> bitshift)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-    }
-    
+        while(++bitshift < 8)
+        {
+            if (message[i] & 0x80 >> bitshift)
+                kill(pid, SIGUSR1);
+            else
+                kill(pid, SIGUSR2);
+            usleep(3);
+        }
+        i++;
+    }  
 }
-
-
-
 
 int main(int argc, char **argv)
 {
