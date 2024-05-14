c321b64515362748b896a810149d0b91dece9e69
rda-cunh <ricard.mendes@gmail.com>
2024-05-15 00:10:39 +0100

    new

--- server.c
+++ server.c
@@ -6,34 +6,38 @@
 /*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 14:06:09 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/14 23:18:13 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/05/15 00:05:02 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h> // remove after testing and including libft functions
 
-void	handler_sigusr2(int signum)
+void handler_sigusr1(int signum) 
 {
-   (void)signum;
-   printf("0");
+    (void)signum;
+    printf("1");
+    fflush(stdout);
 }
 
-void	handler_sigusr1(int signum)
+void handler_sigusr2(int signum) 
 {
-   (void)signum;
-   printf("1");
+    (void)signum;
+    printf("0");
+    fflush(stdout);
 }
 
-int	main(void)
+int main(void) 
 {
-   pid_t	pid;
+    printf("PID = %d\n", getpid());
+    
+    // Set up signal handlers
+    signal(SIGUSR1, handler_sigusr1);
+    signal(SIGUSR2, handler_sigusr2);
 
-   pid = getpid();
-   printf("PID: %d\n", pid);
-   signal(SIGUSR1, handler_sigusr1);
-   signal(SIGUSR2, handler_sigusr2);
-   while (1)
-   	pause ();
-   return (0);
+    // Enter infinite loop waiting for signals
+    while (1)
+        pause();
+
+    return 0;
 }
