7ee60f86e3dfc8933b8d70f1e781ba35ce5262d2
rda-cunh <ricard.mendes@gmail.com>
2024-05-14 18:29:25 +0100

    new

--- client.c
+++ client.c
@@ -6,45 +6,47 @@
 /*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 14:05:48 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/13 18:45:46 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/05/14 18:28:14 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h> // remove after testing and including libft functions
 
-int main(int argc, char **argv)
+void    signal_sender(char *message, int pid)
 {
+    int i;
     int bitshift;
-    char    word; 
-    
-    if (argc != 2)
-        return (1); 
-    word = argv[1][0];
-    bitshift = -1; 
-    while (++bitshift < 8)
+    char word;
+
+    i = 0; 
+    bitshift = -1;
+    while  
+
+
+    while(++bitshift < 8)
     {
         if (word & 0x80 >> bitshift)
-            printf("1");
-        else 
-            printf("0");
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
     }
-    printf("\n");
-    return (0); 
+    
 }
 
-/*
+
+
+
 int main(int argc, char **argv)
 {
     int pid;
 
-    if (argc != 2)
+    if (argc != 3)
     {
         printf("Client: invalid argument.\n");
         exit(EXIT_FAILURE);
     }
     pid = atoi(argv[1]);
-    kill(pid, SIGUSR1);
+    signal_sender(argv[2], pid);
     return (0); 
 }
-*/
--- server.c
+++ server.c
@@ -3,26 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 14:06:09 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/13 13:03:15 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/14 18:28:35 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h> // remove after testing and including libft functions
 
-void    signal_handler(int signum)
+void    handler_sigusr1(int signum)
 {
     (void)signum; 
-    printf("Server: signal received from client.\n");
+    printf("1");
 }
 
-int main(void)
+void    handler_sigusr2(int signum)
+{
+    (void)signum; 
+    printf("0");
+}
+
+int main(void) 
 {
     printf("PID = %d\n", getpid());
-    signal(SIGUSR1, signal_handler);
+    signal(SIGUSR1, handler_sigusr1);
+    signal(SIGUSR2, handler_sigusr2); 
     while (1)
         pause(); 
     return (0); 
