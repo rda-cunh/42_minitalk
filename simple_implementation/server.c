0d69fe3216fe6c8195533804e57a8f6f6d4c3e41
Ricardo Mendes <147749667+rda-cunh@users.noreply.github.com>
2024-05-21 16:46:45 +0000

    new

--- simple_implementation/server.c
+++ simple_implementation/server.c
@@ -5,37 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
+<<<<<<< HEAD
 /*   Created: 2024/05/20 13:16:32 by rda-cunh          #+#    #+#             */
 /*   Updated: 2024/05/20 18:21:31 by rda-cunh         ###   ########.fr       */
+=======
+/*   Created: 2024/05/20 13:16:32 by codespace         #+#    #+#             */
+/*   Updated: 2024/05/21 16:41:44 by codespace        ###   ########.fr       */
+>>>>>>> a602df1 (new)
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h> // remove after changing printf for ft_printf
 
-void    handler_sigusr(int signum)
+void    handler(int signum)
 {
-    static unsigned char buff;
-    static int  i; 
+    static unsigned char c = 0;
+    static int  i = 0; 
 
-    buff |= (signum == SIGUSR1);
+    if (signum == SIGUSR2)
+        c = c << 1;
+    else if (signum == SIGUSR1)
+        c = (c << 1) | 0b00000001;
     i++;
     if (i == 8)
     {
-        printf("%c", buff);
+        printf("%c", c);
         i = 0;
-        buff = 0;
+        c = 0; 
     }
-    else 
-        buff <<= 1;
 }
 
 int main(void)
 {
-    printf("The PID server is: %d\n", getpid());
-    signal(SIGUSR2, handler_sigusr);
-    signal(SIGUSR1, handler_sigusr);
+    printf("My PID is: %d\n", getpid());
     while (1)
-        pause();
+    {
+        signal(SIGUSR2, handler);
+        signal(SIGUSR1, handler);
+    }
     return (0);
 }
