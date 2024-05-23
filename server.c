5475a2bb09383e618448363d6d4a18b15f2979f3
Ricardo Mendes <147749667+rda-cunh@users.noreply.github.com>
2024-05-23 12:39:50 +0000

    new

--- server.c
+++ server.c
@@ -6,11 +6,10 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 13:16:32 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/23 12:36:37 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/23 12:36:54 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// test
 #include "minitalk.h"
 #include <stdio.h> // remove after changing printf for ft_printf
 
