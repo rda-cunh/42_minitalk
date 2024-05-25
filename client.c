f38b12e061b7aec710a1fa01f6fe34d54d203a7c
rda-cunh <ricard.mendes@gmail.com>
2024-05-25 23:31:46 +0100

    norm corrections

--- client.c
+++ client.c
@@ -3,62 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 12:58:21 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/23 13:10:24 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/25 23:31:10 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    send_signals(int pid, unsigned char octet)
+void	send_signals(int pid, unsigned char octet)
 {
-    int i;
+	int	i;
 
-    i = 8;
-    while (i-- > 0)
-    {
-        if (octet & (1 << i))
-        {
-            if (kill(pid, SIGUSR1) == -1)
-            {
-                perror("Error sending SIGUSR1");
-                exit(1);
-            }
-        }
-        else
-        {
-            if (kill(pid, SIGUSR2) == -1)
-            {
-                perror("Error sending SIGUSR2");
-                exit(1);
-            }
-        }
-        usleep(300);
-    }
+	i = 8;
+	while (i-- > 0)
+	{
+		if (octet & (1 << i))
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				perror("Error sending SIGUSR1");
+				exit(1);
+			}
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				perror("Error sending SIGUSR2");
+				exit(1);
+			}
+		}
+		usleep(300);
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int pid;
-    char    *str_to_send;
-    int i;
+	int		pid;
+	char	*str_to_send;
+	int		i;
 
-    if (argc != 3)
-    {
-        ft_printf("Number of arguments provided is not correct.\n");
-        return (1);
-    }
-    pid = ft_atoi(argv[1]);
-    str_to_send = argv[2];
-    i = 0;
-    while (str_to_send[i])
-    {
-        send_signals(pid, (unsigned char)str_to_send[i]);
-        i++;
-    }
-    ft_printf("Number of characters sent: %d\n", i);
-    return (0);
+	if (argc != 3)
+	{
+		ft_printf("Number of arguments provided is not correct.\n");
+		return (1);
+	}
+	pid = ft_atoi(argv[1]);
+	str_to_send = argv[2];
+	i = 0;
+	while (str_to_send[i])
+	{
+		send_signals(pid, (unsigned char)str_to_send[i]);
+		i++;
+	}
+	ft_printf("Number of characters sent: %d\n", i);
+	return (0);
 }
 
--- server.c
+++ server.c
@@ -3,41 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 13:16:32 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/23 13:03:13 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/25 23:25:40 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    handler(int signum)
+void	handler(int signum)
 {
-    static unsigned char c = 0;
-    static int  i = 0; 
+	static unsigned char	c = 0;
+	static int				i = 0;
 
-    if (signum == SIGUSR2)
-        c = c << 1;
-    else if (signum == SIGUSR1)
-        c = (c << 1) | 0b00000001;
-    i++;
-    if (i == 8)
-    {
-        write(STDOUT_FILENO, &c, 1);
-        i = 0;
-        c = 0; 
-    }
+	if (signum == SIGUSR2)
+		c = c << 1;
+	else if (signum == SIGUSR1)
+		c = (c << 1) | 0b00000001;
+	i++;
+	if (i == 8)
+	{
+		write(STDOUT_FILENO, &c, 1);
+		i = 0;
+		c = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    ft_printf("My PID is: %d\n", getpid());
-    signal(SIGUSR2, handler);
-    signal(SIGUSR1, handler);
-    while (1)
-    {
-        pause(); 
-    }
-    return (0);
+	ft_printf("My PID is: %d\n", getpid());
+	signal(SIGUSR2, handler);
+	signal(SIGUSR1, handler);
+	while (1)
+	{
+		pause();
+	}
+	return (0);
 }
