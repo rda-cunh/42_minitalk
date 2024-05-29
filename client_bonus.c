dc3a54f5d3ebb710d8f108c5fca7edbf7b0764ce
Ricardo Mendes <147749667+rda-cunh@users.noreply.github.com>
2024-05-29 13:13:54 +0000

    new

--- client_bonus.c
+++ client_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 12:58:21 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/05/28 23:31:50 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/05/29 13:13:20 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,37 @@ void	send_signals(int pid, unsigned char octet)
 	}
 }
 
+int is_valid_pid(const char *pid)
+{
+    int i = 0;
+    while (pid[i])
+    {
+        if (!ft_isdigit(pid[i]))
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+
 int	main(int argc, char **argv)
 {
-	int		pid;
-	char	*str_to_send;
-	int		i;
+	int					i;
+	struct sigaction	sa_signal;
 
-	if (argc != 3)
+	i = 0;
+	if (argc != 3 || argv[2][0] == '\0' || !is_valid_pid(argv[1])) 
 	{
-		ft_printf("Number of arguments provided is not correct.\n");
+		ft_printf("Incorrect number of arguments, empty message or invalid PID.\n");
 		return (EXIT_FAILURE);
 	}
-	pid = ft_atoi(argv[1]);
-	str_to_send = argv[2];
-	i = 0;
+	else
+	
 	while (str_to_send[i])
 	{
 		send_signals(pid, (unsigned char)str_to_send[i]);
 		i++;
 	}
 	ft_printf("Number of characters sent: %d\n", i);
-	return (0);
+	return (EXIT_SUCCESS);
 }
